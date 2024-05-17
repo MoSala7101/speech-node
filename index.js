58ece823c49c0bec399215c41f8e08cd75f5f60c
sala7webdev <mohamed.salah.webdev@gmail.com>
2024-05-17 17:58:25 +0300

    add live link

--- index.js
+++ index.js
@@ -14,46 +14,46 @@ app.use(cors());
 
 // Endpoint to handle file uploads
 app.post('/upload', upload.single('audio'), async (req, res) => {
-  console.log("File upload request received.");
+    console.log("File upload request received.");
 
-  const filePath = path.join(__dirname, req.file.path);
-  try {
-    const audioBytes = fs.readFileSync(filePath).toString('base64');
-    const audio = {
-      content: audioBytes,
-    };
-
-    const config = {
-      encoding: 'LINEAR16',
-    //   sampleRateHertz: 16000,
-      languageCode: 'en-US',
-    };
-
-    const request = {
-      audio: audio,
-      config: config,
-    };
-
+    const filePath = path.join(__dirname, req.file.path);
     try {
-      const [response] = await speechClient.recognize(request);
-      const transcription = response.results
-        .map(result => result.alternatives[0].transcript)
-        .join('\n');
+        const audioBytes = fs.readFileSync(filePath).toString('base64');
+        const audio = {
+            content: audioBytes,
+        };
 
-      res.json({ transcription });
+        const config = {
+            encoding: 'LINEAR16',
+            //   sampleRateHertz: 16000,
+            languageCode: 'en-US',
+        };
 
-      // Clean up the uploaded file
-      fs.unlinkSync(filePath);
+        const request = {
+            audio: audio,
+            config: config,
+        };
+
+        try {
+            const [response] = await speechClient.recognize(request);
+            const transcription = response.results
+                .map(result => result.alternatives[0].transcript)
+                .join('\n');
+
+            res.json({ transcription });
+
+            // Clean up the uploaded file
+            fs.unlinkSync(filePath);
+        } catch (error) {
+            console.log(error.message);
+            res.status(500).json({ error: error.message });
+        }
     } catch (error) {
-      console.log(error.message);
-      res.status(500).json({ error: error.message });
+        console.log(error);
+        res.status(500).json({ error: error.message });
     }
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: error.message });
-  }
 });
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+    console.log('Server is running on port 3000');
 });
