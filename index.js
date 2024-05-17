0d1b4498f17e3b95f160549cf4681a858b905a9d
sala7webdev <mohamed.salah.webdev@gmail.com>
2024-05-17 18:40:38 +0300

    comment sampleRateHertz

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.post('/upload', upload.single('audio'), async (req, res) => {
 
         const config = {
             encoding: 'LINEAR16',
-            sampleRateHertz: 16000,
+            // sampleRateHertz: 16000,
             languageCode: 'en-US',
         };
 
